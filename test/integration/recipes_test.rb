65b2950c030bfe96d3ad3cb55bffdefc68c1ee42
melnikolaidis <melnikolaidis86@gmail.com>
2017-04-29 19:57:04 +0000

    Completing first section of the application and deploying

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -27,6 +27,9 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @recipe.name, response.body
     assert_match @recipe.description, response.body
     assert_match @chef.chefname, response.body
+    assert_select 'a[href=?]', edit_recipe_path(@recipe), text: "Edit this recipe"
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+    assert_select 'a[href=?]', recipes_path, text: "Return to recipes listing"
   end
   
   test "create new valid recipe" do
@@ -51,6 +54,29 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_template 'recipes/new'
     assert_select 'h2.panel-title'
     assert_select 'div.panel-body'
+  end
+  
+  test "reject invalid recipe update" do
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    patch recipe_path(@recipe), params: { recipe: { name: " ", description: "some description" } } 
+    assert_template 'recipes/edit'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+  
+    test "successfully edit a recipe" do
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    updated_name = "updated recipe name"
+    updated_description = "updated recipe description"
+    patch recipe_path(@recipe), params: { recipe: { name: updated_name, description: updated_description } }
+    assert_redirected_to @recipe
+    #follow_redirect!
+    assert_not flash.empty?
+    @recipe.reload
+    assert_match updated_name, @recipe.name
+    assert_match updated_description, @recipe.description
   end
   
 end
